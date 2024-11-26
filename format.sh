2cc4f057d0a8434275123a82b7bffb6dc07c262b
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-11-26 14:27:04 +0100

    Add format.sh script

---
+++ format.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for filename in ./kern/*.krn; do
+    cat "$filename" \
+        | shed -x "text" -e "s/'/’/g" \
+        | shed -x "kern" -e "s/LL/L/g" \
+        | shed -x "kern" -e "s/JJ/J/g" \
+        | sed 's/=-/=/g' \
+        | sed '/!!linebreak:original/d' \
+        | echo -n "$(cat)" > "$filename.tmp"
+	mv "$filename.tmp" "$filename"
+done
